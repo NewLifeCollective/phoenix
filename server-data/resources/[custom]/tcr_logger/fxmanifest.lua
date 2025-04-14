9d5907d7e3f5282b63897086b47fde74c3347896
CharlieSgsrp <57159677+TheClassyR3dneck@users.noreply.github.com>
2025-04-14 01:21:33 -0500

    added chat logger
    created by TheClassyR3dneck

---
+++ server-data/resources/[custom]/tcr_logger/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheClassyR3dneck'
+description 'Relays in-game chat message to a Discord Channel'
+version '1.0.0'
+
+server_scripts {
+    'server.lua'
+}
+
+dependency 'Badger_Discord_API'
