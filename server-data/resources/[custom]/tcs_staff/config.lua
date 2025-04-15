1cd221a54ce00a135c1aa480379cd14659a4f0e4
CharlieSgsrp <57159677+TheClassyR3dneck@users.noreply.github.com>
2025-04-14 23:51:54 -0500

    updates to staff resource

---
+++ server-data/resources/[custom]/tcs_staff/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- Enable for everyone (for testing), or only if player has permission
+Config.OnlyAllowStaff = true
+
+-- ACE or permission system check (optional if you use Discord roles, etc.)
+Config.Permission = "staffblips.use"
+
+-- Customize blip appearance
+Config.BlipSprite = 1
+Config.BlipScale = 0.8
+Config.BlipColor = 2
