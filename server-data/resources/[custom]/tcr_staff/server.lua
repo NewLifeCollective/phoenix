e5adfe3305cca0ce97b3071dd928138e3a497272
NewLife-Collective <newlifecollectiveofficial@gmail.com>
2025-04-21 21:03:04 -0700

    removed broken staff resource

--- server-data/resources/[custom]/tcr_staff/server.lua
+++
@@ -1,6 +0,0 @@
-RegisterServerEvent('tcr_staff:checkPermission')
-AddEventHandler('tcr_staff:checkPermission', function()
-    local source = source
-    local hasPermission = IsPlayerAceAllowed(source, 'playerblips.use')
-    TriggerClientEvent('tcr_staff:receivePermission', source, hasPermission)
-end)
