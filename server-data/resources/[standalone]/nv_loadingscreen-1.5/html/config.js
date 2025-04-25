6fe7856fae9d072ec08c4bab4203ad58ba7bb1bf
NewLife-Collective <newlifecollectiveofficial@gmail.com>
2025-04-24 20:23:37 -0700

    replaced loading screen

---
+++ server-data/resources/[standalone]/nv_loadingscreen-1.5/html/config.js
@@ -0,0 +1,103 @@
+/*=================================================================*\
+/* By: 			|	Nevera Development  							|
+/* FiveM: 		|	https://forum.cfx.re/u/neveradevelopment		|
+/* Discord: 	|	https://discord.gg/NeveraDev/tw28AqrgWU  		|
+/*=================================================================*/
+/* If you have any problems you can contact us via discord. <3     */
+
+
+var staff_team = [
+
+	{
+		"name":"Barkley",
+		"image":"https://media.discordapp.net/attachments/1347447044318695465/1347699231669878804/NLC_Logo_1.png?ex=680c0e37&is=680abcb7&hm=97cb626a955855eeba0c8082997e20ba2aab1fe6426c1045957dc73bccd94b40&=&format=webp&quality=lossless&width=960&height=960",
+		"rank":"Lead"
+	},
+	{
+		"name":"Charlie",
+		"image":"https://media.discordapp.net/attachments/1347447044318695465/1347699231669878804/NLC_Logo_1.png?ex=680c0e37&is=680abcb7&hm=97cb626a955855eeba0c8082997e20ba2aab1fe6426c1045957dc73bccd94b40&=&format=webp&quality=lossless&width=960&height=960",
+		"rank":"Lead"
+	},
+	{
+		"name":"Pimple",
+		"image":"https://media.discordapp.net/attachments/1347447044318695465/1347699231669878804/NLC_Logo_1.png?ex=680c0e37&is=680abcb7&hm=97cb626a955855eeba0c8082997e20ba2aab1fe6426c1045957dc73bccd94b40&=&format=webp&quality=lossless&width=960&height=960",
+		"rank":"Lead"
+	},
+	{
+		"name":"Ty",
+		"image":"https://media.discordapp.net/attachments/1347447044318695465/1347699231669878804/NLC_Logo_1.png?ex=680c0e37&is=680abcb7&hm=97cb626a955855eeba0c8082997e20ba2aab1fe6426c1045957dc73bccd94b40&=&format=webp&quality=lossless&width=960&height=960",
+		"rank":"Lead"
+	}
+
+]
+
+
+// Staff Settings
+const showStaffTeam = true
+const showPlayersList = true
+
+
+// orange
+// red
+// blue
+// green
+// pink
+// purple
+const theme = "blue"
+
+// ==== WINTER UPDATE !! ==== \\
+const enableWinterUpdate = false
+// ==== WINTER UPDATE !! ==== \\
+
+
+// Text settings
+const name = "<strong>NEW</strong>LIFE"
+const underName = "COLLE<b>CTIVE</b>"
+const desc = "Welcome, we hope you have a good time!"
+
+
+// Social media
+const discord = "https://discord.gg/NLCollective"  // If = "" then icon will not show up on screen
+const instagram = "https://www.instagram.com/newlife_collective/"	
+const youtube = "https://www.youtube.com/@NewLifeCollectiveOfficial" 	
+const twitter = "" 		// https://example.com
+const tiktok = "https://www.tiktok.com/@newlife_collective"  		
+const facebook = ""		// https://example.com
+const twitch = "" 		// https://example.com
+const github = "" 		// https://example.com
+
+
+
+// Player List
+const serverCode = "53dpa6" //Your server CODE from fivem. (cfx.re/join/abcdef) this ABCDEF is your code, paste it in serverCode. 
+const playerProfileImage = "https://media.discordapp.net/attachments/1347447044318695465/1347699231669878804/NLC_Logo_1.png?ex=680c0e37&is=680abcb7&hm=97cb626a955855eeba0c8082997e20ba2aab1fe6426c1045957dc73bccd94b40&=&format=webp&quality=lossless&width=960&height=960"
+
+
+// Video Settings
+const videoBlur = 0
+var videoOpacity = 0.3
+
+
+// Example link: https://www.youtube.com/watch?v=abcdefgh
+const youtubeVideo = "https://www.youtube.com/watch?v=yz3VUMpdjfE"
+const showYoutubeVideo = true
+
+// Local Video
+const enableLocalVideo = false
+
+// Local audio
+const localAudio = false
+
+
+
+// HELP //
+
+//-- YOUTBE
+//-- LOCAL AUDIO
+// if localAudio is true, then loading will load "audio.mp3" file and play it except youtube audio.
+// if localAudio is false, then loading will load youtube audio.
+
+//-- LOCAL VIDEO
+// if enableLocalVideo is true, then loading will load "video.webm" file and play it except youtube video.
+// If localVideo is enabled, showYoutubeVideo is automatically disabled.
+// You can only import a video from either YouTube or local. Local video taking priority.
